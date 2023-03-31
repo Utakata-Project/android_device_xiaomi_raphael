PixelExperience-Devices/device_xiaomi_raphael

raphael: Import missing keylayout
These are needed for volume up and down
 buttons on headphones to work properly

Change-Id: I9f339a1e058385250dd2fdc11dc3ef3a8d0a1394

device.mk
@@ -266,7 +266,8 @@ PRODUCT_COPY_FILES += \
 
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/sm8150-tavil-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sm8150-tavil-snd-card_Button_Jack.kl
 
 # Keymaster
 PRODUCT_PACKAGES += \

Utakata-Project/android_device_xiaomi_raphael

Signed-off-by: onettboots <blackcocopet@gmail.com>
Signed-off-by: SageOfD6Path <mail2anirban95@gmail.com>

device.mk
@@ -260,7 +260,8 @@ PRODUCT_COPY_FILES += \
 
 # Input
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/sm8150-tavil-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/sm8150-tavil-snd-card_Button_Jack.kl
 
 # Keystore
 PRODUCT_PACKAGES += \